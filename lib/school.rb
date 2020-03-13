9bf22d44c350ec24fc0a3a43132fe85027ac4b2c
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-13 09:11:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  
+  attr_reader :school
+  
+  def initialize(school)
+  @school = school  
+  end
+  
+  def roster(grade_level,student_name)
+    roster = {#{grade_level} => [student_name]}
+  end
+ 
+end
