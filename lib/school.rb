0beb97d45079fd1183ffff63a15f0a1ed2178654
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-13 10:23:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,8 @@ class School
   end
   
   def add_student(name, grade)
-    roster[grade]= []
-    roster[grade] << "#{name}"
+    roster["#{grade}"]= []
+    roster["#{grade}"] << "#{name}"
 
   end
   
